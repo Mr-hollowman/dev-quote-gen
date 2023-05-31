a07d10bca83e25b9f451481abf6f6ee2197996e0
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-05-31 20:58:05 +0530

    initial ui setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./App.css";
-
+import axios from "axios";
 export default function App() {
+  const [data, setData] = useState(false);
+  const [error, setError] = useState(null);
+  const getRandomQuote = () => {
+    axios("https://quote-garden.onrender.com/api/v3/quotes/random")
+      .then((res) => setData(res.data.data))
+      .catch((err) => setError(err.message));
+  };
+  useEffect(() => {
+    getRandomQuote();
+  }, []);
+  console.log(data);
   return (
-    <div>
-      
+    <div className="app-container">
+      <div className="quote-container">
+        <div className="quote">{data && data[0].quoteText}</div>
+      </div>
     </div>
-  )
+  );
 }
