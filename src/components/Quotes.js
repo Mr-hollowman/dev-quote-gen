491f93e6f77dd89054fab4aa538a5e6aad8bbc3a
mr-hollowman <92283129+Mr-hollowman@users.noreply.github.com>
2023-05-31 22:15:18 +0530

    updates

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,28 +1,37 @@
 import React from "react";
 
-export default function Quotes({ quoteText, quoteAuthor, quoteGenre }) {
+export default function Quotes({
+  quoteText,
+  quoteAuthor,
+  quoteGenre,
+  isAuthorPage,
+  getRandomQuote,
+}) {
+  console.log(isAuthorPage, "author page");
   return (
     <div className="quote-container">
       <p className="quote">{quoteText}</p>
-      <div className="author">
-        <div className="name-container">
-          <span>{quoteAuthor}</span>
-          <small>{quoteGenre}</small>
+      {!isAuthorPage && (
+        <div className="author" onClick={() => getRandomQuote(quoteAuthor)}>
+          <div className="name-container">
+            <span>{quoteAuthor}</span>
+            <small>{quoteGenre}</small>
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="white"
+            className="bi bi-arrow-right"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fillRule="evenodd"
+              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
+            />
+          </svg>
         </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="white"
-          class="bi bi-arrow-right"
-          viewBox="0 0 16 16"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"
-          />
-        </svg>
-      </div>
+      )}
     </div>
   );
 }
